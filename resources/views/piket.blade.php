b78a4d55ead732f6b45c0b47ecfdbfe9256d4870
Fathan Syarifullah <syarifullahfathan@gmail.com>
2026-03-06 22:38:38 +0700

    nih table jadwal sama piket, gausah pake db jir

--- resources/views/piket.blade.php
+++ resources/views/piket.blade.php
@@ -1,3 +1,97 @@
 <x-layout>
-    <h1>ini halaman piket</h1>
+        <table>
+        <thead>
+            <th>Senin</th>
+            <th>Selasa</th>
+            <th>Rabu</th>
+            <th>Kamis</th>
+            <th>Jumat</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+            <tr>
+                <td>Industri PPLG</td>
+                <td>Bahasa Indonesia</td>
+                <td>IPA</td>
+                <td>IPS</td>
+                <td>Bahasa Inggris</td>
+            </tr>
+        </tbody>
+    </table>
 </x-layout>
