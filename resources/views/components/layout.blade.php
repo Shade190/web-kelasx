aa2eac3c0b31e692bf0b8947f1e9eaed518464bf
fi-suffix <fadildafiamsyah@gmail.com>
2026-03-06 22:37:47 +0700

    bikin css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>RPL 1</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <header>
-        <h1 style="background-color: red">X RPL 1</h1>
+        <div>
+            <div>
+            <h1 style="background-color: red">X RPL 1</h1>
+            <p>Dashboard</p>
+            </div>
     <nav style="background-color: yellow">
         <li><a href="/">Ringkasan</a></li>
         <li><a href="/siswa">Siswa</a></li>
@@ -16,6 +21,7 @@
         <li><a href="/pengumuman">Pengumuman</a></li>
         <li><a href="/piket">Piket</a></li>
     </nav>
+    </div>
     </header>
 
     <main>
--- resources/views/piket.blade.php
+++ resources/views/piket.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
     <h1>ini halaman piket</h1>
+
+    <img src="{{asset('images/abel.jpeg') }}" alt="ouuhhh">
 </x-layout>
