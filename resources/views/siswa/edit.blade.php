3c39bbd231f1f400fba303dddf03d1c6425a6c7c
Fathan Syarifullah <syarifullahfathan@gmail.com>
2026-03-12 11:13:59 +0700

    comment biar ga bingung

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -2,10 +2,13 @@
 <h3 style="margin-bottom: 20px;"><i class="fas fa-bullhorn" style="color: var(--primary-red);"></i> Pengumuman Kelas</h3>
     <form action="{{ route('siswa.update', $siswa->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
+        {{-- pergantian method dari post menjadi put --}}
         @method('PUT')
         <div>
             <label for="nama">nama:</label>
-            <input type="text" id="nama" name="nama" value="{{ old('nama', $siswa->nama) }}" required>
+            <input type="text" id="nama" name="nama" 
+            {{-- value defaultnya adalah data yang lama --}}
+            value="{{ old('nama', $siswa->nama) }}" required>
         </div>
         <div>
             <label for="image">image:</label>
